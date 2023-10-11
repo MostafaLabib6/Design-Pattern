190484d4edbac8b2a22a6ea0f1d7929b76b0a1c3
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-11 23:36:49 +0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Singleton;
+
+public class Program
+{
+    public void main()
+    {
+        Logger instance1 = Logger.GetInstance;
+        Logger instance2 = Logger.GetInstance;
+
+        if(instance1 == instance2)
+            Console.WriteLine("Instances are the same");
+
+    }
+}
