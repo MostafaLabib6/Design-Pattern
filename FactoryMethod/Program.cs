364fae03087cc82bcea9957c87774e16f04ca16f
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-12 23:40:01 +0300

    Test behavior of Factory Method pattern

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using FactoryMethod;
+
+Console.Title = "FactoryMethod";
+
+
+IEnumerable<DiscountFactory> discountFactories = new List<DiscountFactory> {
+                        new CodeDiscountFactory(code:Guid.NewGuid()),
+                        new CountryDiscountFactory("EGY"),
+                        new CodeDiscountFactory(code:Guid.NewGuid()),
+                        new CountryDiscountFactory("BEG")
+                        };
+
+foreach (var discountFactory in discountFactories)
+{
+    discountFactory.GetDiscount();
+    Console.WriteLine(discountFactory);
+}
