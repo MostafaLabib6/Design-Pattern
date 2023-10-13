b4d29518112da5e720676f7c547c6f93bff721ab
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-13 21:53:36 +0300

    add Testing program for Abstract Factory

---
+++ Design Pattern/AbstractFactory/Program.cs
@@ -0,0 +1,18 @@
+﻿using AbstractFactory;
+
+Console.Title = "Abstract Factory";
+
+IEnumerable<IGuiFactory> guiFactories = new List<IGuiFactory>()
+{
+    new WinFactory(),
+    new MacFactory(),
+};
+
+foreach (var guiFactory in guiFactories)
+{
+    IButtonService buttonService = new Application(guiFactory).CreateBtn();
+    ICheckBtnService checkBtnService = new Application(guiFactory).CreateCheckBtn();
+
+    buttonService.print();
+    checkBtnService.print();
+}
