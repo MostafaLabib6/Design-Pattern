5148740272c2aa2e81ea15afd9d48ffb8d8388c1
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-17 00:00:56 +0300

    unit test for builder pattern

---
+++ Design Pattern/Builder/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Builder;
+Console.Title = "Builder Pattern";
+
+Casher casher = new Casher();
+
+IBurgerBuilder Wburger = new WorldWarBurger();
+IBurgerBuilder Oburger = new OriginalBurger();
+
+casher.Constract(Wburger);
+Console.WriteLine(Wburger._burger.ToString());
