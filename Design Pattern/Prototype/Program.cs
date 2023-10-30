9b52a8a00ec097fabeb094228f1c7446e04e04e0
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-30 19:01:00 +0200

    testing prototype pattern

---
+++ Design Pattern/Prototype/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+Console.Title = "Prototype";
+var manager = new Prototype.Manager("Emad");
+var managerClone = manager.Clone();
+Console.WriteLine(manager);
+Console.WriteLine(managerClone);
+
+var employee = new Prototype.Employee("Mostafa", (Prototype.Manager)managerClone);
+var employeeClone = employee.Clone();
+Console.WriteLine(employee);
+Console.WriteLine(employeeClone);
